78b06848c5ea75e62b9e4620648930ac161601f1
erich3576 <erich3576@SURF-SL84145>
2020-11-30 11:15:44 -0800

    src init

---
+++ Breakout/src/elements/Ball.java
@@ -0,0 +1,114 @@
+package elements;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+public class Ball extends Rectangle {
+	public int dx, dy; // Upper Bound is 14, lower bound for y is 2
+	public boolean isServingToRight;
+
+	final int STARTING_X, STARTING_Y, PADDING = 10, MIN_WINDOW = 0, MAX_WINDOW_X = 800, MAX_WINDOW_Y = 600;;
+
+	// Ball constructor to be defined in other classes
+	public Ball(int x, int y, int height, int width, int dx, int dy) {
+		super(x, x, height, width);
+		this.x = x;
+		this.y = y;
+		this.height = height;
+		this.width = width;
+		this.dx = dx;
+		this.dy = dy;
+
+		STARTING_X = x;
+		STARTING_Y = y;
+	}
+
+	// Reset ball to center
+	public void ballReset(Pong p) {
+		this.setLocation(STARTING_X, STARTING_Y);
+		this.dx = 0;
+		this.dy = 0;
+		p.is2PPlaying = false;
+	}
+
+	// Gets random speed
+	public int getSpeed() {
+		int range = 3, min = 3;
+		return (int) (Math.random() * range) + min;
+	}
+
+	// Gets random direction
+	public int getDir() {
+		int rand = (int) (Math.random() * 2);
+		if (rand == 1)
+			return 1;
+		return -1;
+	}
+
+	// Sets velocity by multiplying speed and direction
+	public void setVel(Pong p) {
+		dx = getSpeed() * getDir();
+		dy = getSpeed() * getDir();
+
+		if (isServingToRight) {
+			if (dx < 0)
+				dx *= -1;
+		} else {
+			if (dx > 0)
+				dx *= -1;
+		}
+
+		p.is2PPlaying = true;
+	}
+
+	// Checks ball collision for window edges
+	public void ballBounce(Pong p) {
+		if (this.getMaxX() > MAX_WINDOW_X) {
+			ballReset(p);
+			p.p1Points += 1;
+			isServingToRight = false;
+			p.isRScoreScreen = true;
+		} else if (this.getX() < 0) {
+			ballReset(p);
+			p.p2Points += 1;
+			isServingToRight = true;
+			p.isRScoreScreen = true;
+		}
+
+		boolean checkBounce = (this.getMinY() <= 0) || (this.getMaxY() >= MAX_WINDOW_Y);
+		if (checkBounce) {
+			dy *= -1;
+		}
+	}
+
+	// Ball movement
+	public void update(Pong p) {
+		if (dx < -14)
+			dx = -14;
+		else if (dx > 14)
+			dx = 14;
+
+		if (dy < -14)
+			dy = -14;
+		else if (dx > 14)
+			dy = 14;
+
+		this.translate(dx, dy);
+		ballBounce(p);
+	}
+
+	// Draws ball and center line
+	public Color ballClr = Color.white;
+
+	public void draw(Graphics2D brush) {
+		Rectangle centerLine = new Rectangle((MAX_WINDOW_X / 2), 0, 1, 600);
+		brush.setColor(Color.white);
+		brush.draw(centerLine);
+		brush.fill(centerLine);
+
+		brush.setColor(ballClr);
+		brush.fill(this);
+	}
+
+}
---
+++ Breakout/src/elements/Paddle.java
@@ -0,0 +1,80 @@
+package elements;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+public class Paddle extends Rectangle {
+	final int STARTING_X, STARTING_Y, PADDING = 10, MIN_WINDOW = 0, MAX_WINDOW_X = 800, MAX_WINDOW_Y = 600;
+	public int dx, dy;
+	public int isMovingUp = 0;
+
+	// Paddle constructor to be defined in other classes
+	public Paddle(int x, int y, int height, int width, int dx, int dy) {
+		super(x, y, height, width);
+		this.x = x;
+		this.y = y;
+		this.height = height;
+		this.width = width;
+		this.dx = dx;
+		this.dy = dy;
+
+		STARTING_X = x;
+		STARTING_Y = y;
+	}
+
+	// Resets paddle location at end of a rally
+	public void padReset() {
+		this.setLocation(STARTING_X, STARTING_Y);
+		this.dy = 0;
+	}
+
+	// Moves the paddle and checks for edge
+	public void move() {
+		this.translate(dx, dy);
+		if (this.getMinY() < MIN_WINDOW) {
+			this.setLocation(x, 0);
+		} else if (this.getMaxY() > MAX_WINDOW_Y) {
+			this.setLocation(x, MAX_WINDOW_Y - this.height);
+		}
+	}
+
+	// Checks for ball paddle collision
+	public void ballPadCol(Ball b) {
+		boolean inYRange = (b.getMaxY() >= this.getMinY()) && (b.getMinY() <= this.getMaxY());
+		boolean isAtFront = (((b.getMinX() >= this.getCenterX()) && (b.getMinX() <= PADDING + this.width))
+				&& (b.dx < 0))
+				|| (((b.getMaxX() <= this.getCenterX()) && b.getMaxX() >= MAX_WINDOW_X - this.width) && (b.dx > 0));
+
+		if (inYRange) {
+			if (isAtFront) {
+				if (this.dy == 0) {
+					b.dx *= -1.15;
+				} else if (((this.dy > 0) && (b.dy > 0)) || ((this.dy < 0) && (b.dy < 0))) {
+					b.dx *= -1.2;
+					b.dy *= 1.5;
+				} else if (((this.dy < 0) && (b.dy > 0)) || ((this.dy > 0) && (b.dy < 0))) {
+					b.dx *= -1.35;
+					b.dy *= -0.86;
+					if (Math.abs(b.dy) < 2) {
+						b.dy = 2;
+					}
+				}
+			}
+		}
+	}
+
+	// calls movement
+	public void update() {
+		move();
+	}
+
+	// Draws paddle object
+	public void draw(Graphics2D win) {
+		Color padClr = Color.white;
+
+		win.setColor(padClr);
+		win.fill(this);
+	}
+
+}
